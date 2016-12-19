a79356fcca20623c7aca00aa960bfbe9c3dbbcdd
JasJaxon <1jasmynejackson@gmail.com>
2016-12-19 02:28:05 -0500

    Chapter 2: Generating authentic star reviews

--- Yelp Reviews.R
+++ Yelp Reviews.R
@@ -89,3 +89,77 @@ ggplot(new_review_indian, aes(x=1:nrow(new_review_indian), y=diff, fill=city)) +
 # Display a summary of the 
 summary(new_review_indian)
 
+# Exploring the data again
+# This url contains the .txt file with Indian names. (Note: Don't change this code)
+indian_names_url <-url("http://s3.amazonaws.com/assets.datacamp.com/production/course_1073/datasets/indian_names.txt")
+
+# Read Indian names into a list
+indian_names <- scan(indian_names_url, what = character())
+
+# Show the first 10 names from the indian_names list
+head(indian_names, 10)
+
+# Clean the names list
+
+# Show the names list
+unlist(indian_names)
+
+# Locate the names that you want to eliminate
+indian_names_remove <- grep("[A-z]\\.", indian_names, perl = TRUE)
+
+# Check to make sure they are the correct names
+indian_names[indian_names_remove]
+
+# Eliminate them from the indian_names list
+indian_names_clean <- indian_names[-indian_names_remove]
+
+# Display a table of the cleaned names list
+unlist(indian_names_clean)
+
+Find the authentic users
+
+# The package `dplyr` is available to use
+# Subset the `indian` data set to just the users with native Indian names
+authentic_users <- subset(indian, indian$user_name %in% indian_names_clean)
+
+# Find the number of users in each city
+number_authentic_city <- authentic_users %>%
+  select(city,user_name) %>%
+  group_by(city) %>%
+  summarise(users = n())
+
+# Generate the average of authentic reviews
+
+# Print the number of users per city
+number_authentic_city
+
+# The package `dplyr` is available to use
+# Generate new "immigrant" review
+avg_review_indian <- authentic_users %>% 
+  select(business_id, business_name, city, stars, 
+         avg_stars, is_indian, user_name) %>%
+  group_by(city, business_name, avg_stars) %>%
+  summarise(count = n(),
+            new_stars = sum(stars) / count) %>%
+  mutate(diff = new_stars - avg_stars)
+
+# Detecting manipulation effect
+
+# The plotting package `ggplot2` is avaibale to use
+# Create a histogram of the avg_stars
+hist(avg_review_indian$avg_stars)
+
+# Create a histogram of the new_stars
+hist(avg_review_indian$new_stars)
+
+# Plot the distribution of changes to reviews 
+hist(avg_review_indian$diff, main = "Changes in Star Reviews", xlab = "Change")
+
+# Plot the changes to per restaurant 
+ggplot(avg_review_indian, aes(x=1:nrow(avg_review_indian), y = diff, fill = city)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  theme_classic() + scale_fill_grey() + xlab("Businesses ID") + ylab("Change in Star Review”)
+                                                                     
+                                                                     
+                                                                     
+
